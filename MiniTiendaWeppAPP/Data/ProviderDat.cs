e8b2c276da5ea95b35e54229c61982b9bbb8a868
Sergio Ortiz <yhersua08@gmail.com>
2024-10-19 16:50:28 -0500

    Se crea la clase proveedor con los cuatro metodos

---
+++ MiniTiendaWeppAPP/Data/ProviderDat.cs
@@ -0,0 +1,119 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Data
+{
+    public class ProviderDat
+    {
+        Persistence objPer = new Persistence();
+
+        // Método para mostrar todos los proveedores desde la base de datos.
+        public DataSet showProveedores()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spSelectProveedores";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+            objPer.closeConnection();
+            return objData;
+        }
+
+        // Método para guardar un nuevo proveedor.
+        public bool saveProveedor(string _nit, string _nombre)
+        {
+            bool executed = false;
+            int row;
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spInsertProveedor";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Agregar parámetros para el NIT y el nombre del proveedor.
+            objSelectCmd.Parameters.Add("p_nit", MySqlDbType.VarString).Value = _nit;
+            objSelectCmd.Parameters.Add("p_nombre", MySqlDbType.VarString).Value = _nombre;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        // Método para actualizar un proveedor.
+        public bool updateProveedor(int _id, string _nit, string _nombre)
+        {
+            bool executed = false;
+            int row;
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spUpdateProveedor";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Agregar los parámetros para actualizar el proveedor.
+            objSelectCmd.Parameters.Add("p_id", MySqlDbType.Int32).Value = _id;
+            objSelectCmd.Parameters.Add("p_nit", MySqlDbType.VarString).Value = _nit;
+            objSelectCmd.Parameters.Add("p_nombre", MySqlDbType.VarString).Value = _nombre;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        // Método para eliminar un proveedor.
+        public bool deleteProveedor(int _id)
+        {
+            bool executed = false;
+            int row;
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spDeleteProveedor";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Agregar parámetro del ID del proveedor a eliminar.
+            objSelectCmd.Parameters.Add("p_id", MySqlDbType.Int32).Value = _id;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+    }
+}
